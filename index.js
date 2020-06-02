4fa827b5cedd1ea86783ebbcd1b534329d545ff6
hajindvlp <harryhajinchung@gmail.com>
2020-06-02 14:42:03 +0900

    Fix bug ( ../ )

--- index.js
+++ index.js
@@ -9,14 +9,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/upload', (req, res) => {
-  console.log(req.body.fn);
-  let fileDest = __dirname + '/doc/' + req.body.fn;
+  let fn = req.body.fn.split("..").join("");
+  let fileDest = __dirname + '/doc/' + fn;
   let fileContent = req.body.content;
 
-  console.log(fileDest, fileContent);
   fs.writeFile(fileDest, fileContent, (err) => {
     if(err) res.status(500);
-    console.log(`File ${req.body.fn} is saved`);
+    console.log(`File ${fileDest} is saved`);
     res.status(200);
   });
   res.status(200);
@@ -34,11 +33,9 @@ app.get('/ls', (req, res) => {
 app.post('/cat', (req, res) => {
   let fn = req.body.fn;
   let dest = `doc/${fn}`;
-  console.log(fn);
 
   fs.readFile(dest, 'utf8', (err, data) => {
     if(err) res.status(500);
-    console.log(data);
     res.status(200).json({
       content: data
     })
